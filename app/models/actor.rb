71e0730552279cbd63a2233a926736e382fc096e
Trillian <trillianjosea@gmail.com>
2018-04-02 14:09:11 -0500

    Crear actor

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,12 @@
 class Actor < ActiveRecord::Base
+  validates :image_url, :name, :bio, :birth_date, :birth_place, presence: true
+  validates :death_date, :death_place, presence: true, if: :is_dead?
+
+  def is_dead?
+    if alive == "0"
+      false
+    else
+      true
+    end
+  end
 end
