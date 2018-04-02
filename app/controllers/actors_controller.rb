eefd08e5e3c1a6638211bee2c1a886d0bf56d68d
Trillian <trillianjosea@gmail.com>
2018-04-02 11:23:22 -0500

    Creating actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,22 @@
 class ActorsController < ApplicationController
+
   def index
     @actors = Actor.all
+  end
+
+  def create
+    @actors = Actor.new(actors_params)
+    @actors.save
+    redirect_to index
+  end
+
+  def new
+    @actors = Actor.new
+  end
+
+  private
+
+  def actors_params
+    params.require(:actors).permit(:image_url, :name, :birth_date, :birth_place, :alive, :death_date, :death_place)
   end
 end
